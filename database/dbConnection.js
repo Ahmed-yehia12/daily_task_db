e523655ea36b797acceb929aee5919c6e30fc11d
Ahmed-yehia12 <ahmed.yehia.abdulgawad@gmail.com>
2024-12-15 02:25:58 +0200

    fix

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 
 
 export function mongoConection(){
-mongoose.connect(process.env.DB_CONNECTION)
+mongoose.connect("mongodb+srv://User:cDCmn6YT6PZS7NXM@cluster0.a6vrp.mongodb.net/daily_task")
 .then(()=>{console.log("Mongo is running too ..")})
 .catch((err)=>{console.log("DataBase error", err)})
 }
